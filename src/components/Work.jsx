6e4f7e8c2596e7abee59a9cb4a3d7ecc8c30d84d
Hasansattar <hasansattar650@gmail.com>
2023-07-10 23:37:26 +0500

    3D portfolio website

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -47,7 +47,7 @@ const Works = () => {
 
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant(0.2)}>
         <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">My work</p>
         <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Projects.</h2>
       </motion.div>
