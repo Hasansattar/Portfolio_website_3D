8d7c31973f4e0029a663254e688a2992dd39b182
Hasansattar <hasansattar650@gmail.com>
2023-07-10 23:46:39 +0500

    3D portfolio website

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -47,14 +47,16 @@ const Works = () => {
 
   return (
     <>
-      <motion.div variants={textVariant(0.2)}>
+    {/* variants={textVariant(0.2)} */}
+      <motion.div >
         <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">My work</p>
         <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]">Projects.</h2>
       </motion.div>
 
       <div className="w-full flex">
+      {/* variants={fadeIn("", "", 0.1, 1)} */}
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
